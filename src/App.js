a0012ce13d58652b2c185590013e8879f8c5413f
hoofacProgram <hoofac.program@gmail.com>
2022-12-19 23:04:33 +0900

    5강 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = (props) => {
+  return <header>
+    <h1><a href='/'>{props.title}</a></h1>
+  </header>
+}
+
+const Nav = (props) => {
+  const lis = props.topics.map(t => 
+    <li key={t.id}><a href={`/read/${t.id}`}>{t.title}</a></li>
+  )
+  return <ol>
+    {lis}
+  </ol>
+}
+
+const Article = (props) => {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is...'},
+    {id:2, title:'css', body:'css is...'},
+    {id:3, title:'javascript', body:'javascript is...'}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title='Web'></Header>
+      <Nav topics={topics}></Nav>
+      <Article title='Welcome' body='Hello, WEB'></Article>
     </div>
-  );
+
+    );
 }
 
 export default App;
